f8a00ee06c08d1359d603f2599f75873257d635e
pyneng <pyneng.course@gmail.com>
2017-06-24 10:48:14 +0000

    Update 09_regex after lecture

--- examples/09_regex/parse_dhcp_snooping.py
+++ examples/09_regex/parse_dhcp_snooping.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 import re
 
-regex = re.compile('(?P<mac>.+?) +(?P<ip>.*?) +(\d+) +([\w-]+) +(?P<vlan>\d+) +(?P<int>.*$)')
+#'00:09:BB:3D:D6:58   10.1.10.2        86250       dhcp-snooping   10    FastEthernet0/1'
+regex = re.compile('(?P<mac>\S+) +(?P<ip>\S+) +\d+ +\S+ +(?P<vlan>\d+) +(?P<port>\S+)')
 result = []
 
 with open('dhcp_snooping.txt') as data:
     for line in data:
-        if line[0].isdigit():
-            result.append(regex.search(line).groupdict())
+        match = regex.search(line)
+        if match:
+            result.append(match.groupdict())
 
-print("К коммутатору подключено {} устройства".format( len(result) ))
+print("К коммутатору подключено {} устройства".format(len(result)))
 
 for num, comp in enumerate(result, 1):
-    print("Параметры устройства {}:".format( num ))
+    print("Параметры устройства {}:".format(num))
     for key in comp:
-        print("{:10}: {:10}".format( key,comp[key] ))
+        print("{:10}: {:10}".format(key,comp[key]))
 
 
 """
