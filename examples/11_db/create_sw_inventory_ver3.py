ad0b8bb9ad1af8ae6290b77cb3b4f92b3bb9a2c2
natenka <nataliya.samoylenko@gmail.com>
2017-07-14 14:55:40 +0000

    Update sqlite examples to py3 and update chapter content

--- examples/11_db/create_sw_inventory_ver3.py
+++ examples/11_db/create_sw_inventory_ver3.py
@@ -1,38 +1,28 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
 import sqlite3
+import create_sw_inventory_ver2_functions as dbf
 
-data = [('0000.AAAA.CCCC', 'sw1', 'Cisco 3750', 'London, Green Str'),
-        ('0000.BBBB.CCCC', 'sw2', 'Cisco 3780', 'London, Green Str'),
-        ('0000.AAAA.DDDD', 'sw3', 'Cisco 2960', 'London, Green Str'),
-        ('0011.AAAA.CCCC', 'sw4', 'Cisco 3750', 'London, Green Str')]
-
-con = sqlite3.connect('sw_inventory3.db')
-con.execute("create table switch (mac text primary key, hostname text, model text, location text)")
-
-try:
-    with con:
-        query = "INSERT into switch values (?, ?, ?, ?)"
-        con.executemany(query, data)
-except sqlite3.IntegrityError as e:
-    print("Error occured: ", e)
-
-for row in con.execute("select * from switch"):
-    print(row)
-
-print('-'*30)
-
-#MAC-адрес sw7 совпадает с MAC-адресом существующего коммутатора - sw3
+#MAC-адрес sw7 совпадает с MAC-адресом коммутатора sw3 в списке data
 data2 = [('0055.AAAA.CCCC', 'sw5', 'Cisco 3750', 'London, Green Str'),
          ('0066.BBBB.CCCC', 'sw6', 'Cisco 3780', 'London, Green Str'),
          ('0000.AAAA.DDDD', 'sw7', 'Cisco 2960', 'London, Green Str'),
          ('0088.AAAA.CCCC', 'sw8', 'Cisco 3750', 'London, Green Str')]
 
-try:
-    with con:
-        query = "INSERT into switch values (?, ?, ?, ?)"
-        con.executemany(query, data2)
-except sqlite3.IntegrityError as e:
-    print("Error occured: ", e)
+con = dbf.create_connection('sw_inventory3.db')
 
-for row in con.execute("select * from switch"):
-    print(row)
+query_insert = "INSERT into switch values (?, ?, ?, ?)"
+query_get_all = "SELECT * from switch"
+
+print("\nПроверка текущего содержимого БД")
+pprint(dbf.get_all_from_db(con, query_get_all))
+
+print('-'*60)
+print("Попытка записать данные с повторяющимся MAC-адресом:")
+pprint(data2)
+dbf.write_data_to_db(con, query_insert, data2)
+print("\nПроверка содержимого БД")
+pprint(dbf.get_all_from_db(con, query_get_all))
+
+con.close()
+
