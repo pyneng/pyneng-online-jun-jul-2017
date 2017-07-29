ebd56f56a1ed1fedba2204f0bc93212df70cf050
pyneng <pyneng.course@gmail.com>
2017-07-29 10:08:58 +0000

    Update after lecture

--- examples/13_jinja2/3_template_syntax/cfg_gen.py
+++ examples/13_jinja2/3_template_syntax/cfg_gen.py
@@ -3,13 +3,14 @@ from jinja2 import Environment, FileSystemLoader
 import yaml
 import sys
 
-TEMPLATE_DIR, template = sys.argv[1].split('/')
+#$ python cfg_gen.py templates/for.txt data_files/for.yml
+TEMPLATE_DIR, template_file = sys.argv[1].split('/')
 VARS_FILE = sys.argv[2]
 
 env = Environment(loader = FileSystemLoader(TEMPLATE_DIR),
                   trim_blocks=True, lstrip_blocks=True)
-template = env.get_template(template)
+template = env.get_template(template_file)
 
-vars_dict = yaml.load( open( VARS_FILE ) )
+vars_dict = yaml.load(open(VARS_FILE))
 
-print(template.render( vars_dict ))
+print(template.render(vars_dict))
