ffe1fc5a41d14b156c863ab9c636923f7ef31062
natenka <nataliya.samoylenko@gmail.com>
2017-08-11 15:01:56 +0000

    Обновила описание заданий 13_jinja2

--- exercises/13_jinja2/task_13_1b.py
+++ exercises/13_jinja2/task_13_1b.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""
+'''
 Задание 13.1b
 
 Дополнить функцию generate_cfg_from_template из задания 13.1 или 13.1a:
@@ -13,5 +13,5 @@
 * trim_blocks
 * lstrip_blocks
 
-"""
+'''
 
